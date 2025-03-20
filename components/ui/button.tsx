f55997ea8848986e9a5e26f20c3a372abe460cfb
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2025-03-20 01:18:41 -0400

    Fixing bugs

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -39,18 +39,19 @@ export interface ButtonProps
   asChild?: boolean
 }
 
+// Remove the import statement for ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
+);
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
